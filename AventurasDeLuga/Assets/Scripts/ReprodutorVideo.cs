2d81b76f0a5bb37f8bc1cb197f6394a788a70771
ADF Games <fonseca.deivid@gmail.com>
2015-08-13 17:37:52 -0300

    Colocado os audio e chamada de cenas

--- AventurasDeLuga/Assets/Scripts/ReprodutorVideo.cs
+++ AventurasDeLuga/Assets/Scripts/ReprodutorVideo.cs
@@ -5,7 +5,7 @@ public class ReprodutorVideo : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Handheld.PlayFullScreenMovie("SPLASH_MP4.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+//        Handheld.PlayFullScreenMovie("SPLASH_MP4.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
 	}
 	
 	// Update is called once per frame
--- AventurasDeLuga/Assets/Scripts/Splash.cs
+++ AventurasDeLuga/Assets/Scripts/Splash.cs
@@ -1,58 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof(AudioSource))]
-
-public class Splash : MonoBehaviour {
-	//public string movie;
-    // Use this for initialization
+public class splash : MonoBehaviour {
+	public float pass = 1f;
+	// Use this for initialization
 	void Start () {
-		//Renderer r = GetComponent<Renderer>();
-		//MovieTexture movie = (MovieTexture)r.material.mainTexture;
-
-		//RawImage rim = GetComponent<RawImage>();
-		//video = (MovieTexture)rim.mainTexture;
-
-    #if UNITY_ANDROID
-		//Handheld.PlayFullScreenMovie("Sequence 03_1", Color.black, FullScreenMovieControlMode.CancelOnInput);
-		Handheld.PlayFullScreenMovie("Sequence 03_1");
-		//Application.LoadLevel("tela_loading");
-		//MovieTexture r = GetComponent<Renderer>().material.mainTexture as MovieTexture;
-		//GetComponent<AudioSource>().clip = r.audioClip;
-		//GetComponent<AudioSource>().Play ();
-		//r.Play ();
-
-		//Application.LoadLevel("tela_loading");
-	#endif
-
-	#if UNITY_STANDALONE_WIN
-
-        AudioSource audio = GetComponent<AudioSource>();
-        //GetComponent<Renderer>().material.mainTexture = video as MovieTexture;
-        audio.clip = video.audioClip;
-        video.Play();
-        audio.Play();
-        
-    #endif
+		Cursor.visible = false;
+		StartCoroutine ("Espera");
 	}
-	
-	// Update is called once per frame
-	void OnMouseDown()
-    {
-    #if UNITY_STANDALONE_WIN
-        video.Stop();
-        Application.LoadLevel("tela_loading");
-	#endif
-    }
 
-
-    void Update()
-    {
-    #if UNITY_STANDALONE_WIN
-        if (!video.isPlaying)
-        {
-            Application.LoadLevel("tela_loading");
-        }
-	#endif
-    }
+	IEnumerator Espera(){
+		yield return new WaitForSeconds (5.5f);
+		Cursor.visible = true;
+		Application.LoadLevel (1);
+	}
 }
